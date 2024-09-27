de435885f5c0eea73379e1699fcda1dd5ea30b09
Piyush Channawar <channawarpiyush@gmail.com>
2024-09-27 15:26:16 +0530

    channel creation

--- components/modals/create-channel-modal.tsx
+++ components/modals/create-channel-modal.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import qs from "query-string"
 import axios from "axios"
 import { useForm } from "react-hook-form";
 import * as z from "zod";
@@ -7,7 +8,6 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
@@ -22,34 +22,52 @@ import {
   FormMessage,
 } from "../ui/form";
 
+
+
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
-import { FileUpload } from "../file-upload";
-import { useRouter } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 import { useModal } from "@/hooks/use-modal-store";
+
+
+import { 
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue
+ } from "../ui/select";
+import { ChannelType } from "@prisma/client";
+
+
+
 
 const formSchema = z.object({
   name: z.string().min(1, {
-    message: "Server name is required.",
-  }),
-  imageUrl: z.string().min(1, {
-    message: "Server image is required.",
-  }),
+    message: "Channel name is required.",
+  }).refine(
+    name => name !== "general",
+    {
+      message: "Channel name cannot be 'general'"
+    }
+  ),
+  type: z.nativeEnum(ChannelType)
 });
 
-export const CreateServerModal = () => {
+export const CreateChannelModal = () => {
 
     const { isOpen, onClose, type} = useModal();
   const router = useRouter();
+  const params = useParams();
 
-  const isModalOpen = isOpen && type === "createServer";
+  const isModalOpen = isOpen && type === "createChannel";
 
   // Call all hooks unconditionally, even before the component is mounted.
   const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
       name: "",
-      imageUrl: "",
+      type: ChannelType.TEXT,
     },
   });
 
@@ -57,7 +75,13 @@ export const CreateServerModal = () => {
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
-      await axios.post("/api/servers",values);
+      const url = qs.stringifyUrl({
+        url: "/api/channels",
+        query: {
+          serverId: params?.serverId
+        }
+      })
+      await axios.post(url,values);
 
       form.reset();
       router.refresh();
@@ -77,53 +101,65 @@ export const CreateServerModal = () => {
       <DialogContent className="bg-white text-black p-0 overflow-hidden">
         <DialogHeader className="pt-8 px-6">
           <DialogTitle className="text-2xl text-center font-bold">
-            Create your Classroom
+            Create your Classroom Channel
           </DialogTitle>
-          <DialogDescription className="text-center text-zinc-500">
-            Give your classroom a name and an image
-          </DialogDescription>
         </DialogHeader>
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
             <div className="space-y-8 px-6">
-              <div className="flex items-center justify-center text-center">
-                <FormField
-                control={form.control}
-                name="imageUrl"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormControl>
-                     <FileUpload 
-                      endpoint="serverImage"
-                      value={field.value}
-                      onChange={field.onChange}
-
-                     />
-                    </FormControl>
-                  </FormItem>
-                )}
-                />
-              </div>
-
               <FormField
                 control={form.control}
                 name="name"
                 render={({ field }) => (
                   <FormItem>
                     <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
-                      Classroom name
+                      Channel name
                     </FormLabel>
                     <FormControl>
                       <Input
                         disabled={isLoading}
                         className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
-                        placeholder="Enter Classroom name"
+                        placeholder="Enter Channel name"
                         {...field}
                       />
                     </FormControl>
                     <FormMessage />
                   </FormItem>
                 )}
+              />
+              <FormField 
+              control={form.control}
+              name="type"
+              render={({ field }) => (
+                <FormItem>
+                      <FormLabel>Channel Type</FormLabel>
+                      <Select
+                      disabled={isLoading}
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                      >
+                        <FormControl>
+                          <SelectTrigger
+                          className="bg-zinc-300/50 border-0 focus:ring-0 text-black ring-offset-0 focus:ring-offset-0 capitalize outline-none"
+                          >
+                            <SelectValue placeholder="Select a channel type" />
+                          </SelectTrigger>
+                        </FormControl>
+                        <SelectContent>
+                          {Object.values(ChannelType).map((type)=>(
+                            <SelectItem
+                            key={type}
+                            value={type}
+                            className="capitalize"
+                            >
+                              {type.toLowerCase()}
+                            </SelectItem>
+                          ))}
+                        </SelectContent>
+                      </Select>
+                      <FormMessage />
+                </FormItem>
+              )}
               />
             </div>
             <DialogFooter className="bg-gray-100 px-6 py-4">
